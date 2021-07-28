e2d3652a75e0829eb2b52f3012bc75eb9705867d
ycssona <ycssona@gmail.com>
2021-07-28 19:49:08 +0530

    Binary Search

---
+++ Search/BinarySearch/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+// NOTE - Binary search works only on sorted array
+
+int binarySearch(int arr[], int left, int right, int x) {
+
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2; // (left + right)/2 would do the same
+
+		if (arr[mid] == x)
+		{
+			return mid;
+		}
+		else if (arr[mid] < x) { // right half
+			left = mid + 1;
+		}
+		else { // left half
+			right = mid - 1;
+		}
+	}
+
+	return -1; // if not found
+}
+
+int main()
+{
+	int myArray[10] = { 1,3,12,14,23,34,55,65,75,78 };
+	int output,num;
+
+	cout << "Array is: " << endl;
+
+	for (int i = 0; i <10; i++) {
+		cout << myArray[i]<<" ";
+	}
+	cout << endl << endl;
+
+	cout << "Enter element to search: ";
+	cin >> num;
+
+	output = binarySearch(myArray, 0, 9, num);
+
+	if (output == -1)
+	{
+		cout << "Element not found" << endl;
+	}
+	else {
+		cout << "Element found at position " << output + 1 << endl;
+	}
+}
